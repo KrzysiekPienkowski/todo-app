2023d06019bd25d44cdae14537d6bfa0e617ee6f
Krzysiek <krzysiek.pienkowski@gmail.com>
2021-01-05 23:55:06 +0100

    add jpa dependency + first entity

---
+++ src/main/java/com/kp/spring/todoapp/model/Task.java
@@ -0,0 +1,17 @@
+package com.kp.spring.todoapp.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name="tasks")
+public class Task {
+    @Id
+    private int id;
+    private String description;
+    private boolean done;
+}
