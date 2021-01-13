b1bc086d6874214cae95dab7c7585ba73f4faff0
Krzysiek <krzysiek.pienkowski@gmail.com>
2021-01-13 21:36:58 +0100

    Main changes

--- src/main/java/com/kp/spring/todoapp/model/Task.java
+++ src/main/java/com/kp/spring/todoapp/model/Task.java
@@ -11,8 +11,38 @@ import javax.persistence.Table;
 @Table(name="tasks")
 public class Task {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotBlank
     private String description;
     private boolean done;
+    private boolean main;
     private int branch;
+
+    Task(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
 }
