1c61b1be22ef1c3b3feace2546a922671084e33e
KrzysiekPienkowski <krzysiek.pienkowski@gmail.com>
2021-01-13 21:44:10 +0100

    Update Task.java
    new main commit

--- src/main/java/com/kp/spring/todoapp/model/Task.java
+++ src/main/java/com/kp/spring/todoapp/model/Task.java
@@ -43,4 +43,30 @@ public class Task {
     public void setDone(boolean done) {
         this.done = done;
     }
+
+    Task(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
 }
