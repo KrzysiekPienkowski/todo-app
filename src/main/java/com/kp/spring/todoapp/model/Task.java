53cccea23545a9096415ef88e6e99d189e6ecd71
Krzysiek <krzysiek.pienkowski@gmail.com>
2021-01-13 21:36:58 +0100

    Main changes

--- src/main/java/com/kp/spring/todoapp/model/Task.java
+++ src/main/java/com/kp/spring/todoapp/model/Task.java
@@ -13,9 +13,37 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotBlank
     private String description;
     private boolean done;
     private int branch;
+    private boolean main;
+
+    Task(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
     private boolean main_new;
     private int main_main;
     private boolean main;
